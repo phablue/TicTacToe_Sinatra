ce8fb5492f59672f7540cccfb6e4fb9f33564f22
hyunahpark <phablue@gmail.com>
2014-03-26 15:08:56 -0500

    correct spelling

--- lib/Human.rb
+++ lib/Human.rb
@@ -1,17 +1,17 @@
 class Human
-	attr_accessor :choosen_spot
+	attr_accessor :chosen_spot
 
 	def initialize
-		@choosen_spot = ""
+		@chosen_spot = ""
 	end
 
 	def choose_spot(board, current_player)
-		check_available(board, current_player, @choosen_spot)
+		check_available(board, current_player, @chosen_spot)
 	end
 
-	def check_available(board, current_player, choosen_spot)
-		if board.available_spots.include?(@choosen_spot)
-			board.mark_choice_spot(@choosen_spot, current_player)
+	def check_available(board, current_player, chosen_spot)
+		if board.available_spots.include?(@chosen_spot)
+			board.mark_choice_spot(@chosen_spot, current_player)
 		end
 	end
 end
