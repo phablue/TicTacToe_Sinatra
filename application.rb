74169b4dfcd507863ac99e0e549785fd7da0b393
hyunahpark <phablue@gmail.com>
2014-03-26 14:57:06 -0500

    add human class in application

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ require "sinatra"
 require "./lib/tictactoe"
 
 configure do
-  set :game => Game.new(Reader.new, Writer.new)
+  set :game => Game.new(Reader.new, Writer.new), :human => Human.new
 end
 
 get "/main" do
@@ -16,4 +16,7 @@ end
 post "/game/choice_mark/" do
   mark = request.body.read
   settings.game.current_player = mark
+end
+
+post "/game/play/" do
 end
