531a29c2efc083f31593efd454e4e1bc1cc53eac
hyunahpark <phablue@gmail.com>
2014-03-26 14:56:15 -0500

    make global choosen_spot variable in Human

--- lib/Human.rb
+++ lib/Human.rb
@@ -1,11 +1,17 @@
 class Human
-	def choose_spot(board, current_player)
-		check_available(board, current_player, answer)
+	attr_accessor :choosen_spot
+
+	def initialize
+		@choosen_spot = ""
 	end
 
-	def check_available(board, current_player, answer)
-		if board.available_spots.include?(answer)
-			board.mark_choice_spot(answer, current_player)
+	def choose_spot(board, current_player)
+		check_available(board, current_player, @choosen_spot)
+	end
+
+	def check_available(board, current_player, choosen_spot)
+		if board.available_spots.include?(@choosen_spot)
+			board.mark_choice_spot(@choosen_spot, current_player)
 		end
 	end
 end
