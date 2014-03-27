20348cb17196fa7750271a23bd98503ee9287eff
hyunahpark <phablue@gmail.com>
2014-03-27 10:08:01 -0500

    add Game and UI JS

---
+++ public/javascripts/Game.js
@@ -0,0 +1,76 @@
+(function() {
+  var Game = {
+    goFirst: null,
+
+    changeCurrentPlayer: function(currentPlayer) {
+      this.currentPlayer = currentPlayer == "X" ? "O" : "X";
+    },
+
+    winner: function(currentPlayer) {
+      if(UI.gameType === ".player") {
+        return currentPlayer == "X" ? "Player" : "Computer";
+      }
+      return currentPlayer == "X" ? "Player 'X'" : "Player 'O'";
+    },
+
+    firstMove: function() {
+      var input = UI.askFirstMove();
+      if(input == "y" || input == "n") {
+        UI.toggleDisplayedButton(".btn-restart", ".btn-new");
+        this.goFirst = input;
+      }
+      else {
+        UI.inputErrorMessage();
+        this.firstMove();
+      }
+    },
+
+    checkGameOver: function(currentPlayer) {
+      Game.changeCurrentPlayer(currentPlayer);
+      if(GameRules.gameOver(GameBoard)) {
+        UI.visualWhenGameOver(currentPlayer);
+        return true;
+      }
+      return false;
+    },
+
+    humanChoice: function(chosenSpotID, callback) {
+      if (Human.choiceSpot(GameBoard, chosenSpotID, UI.currentPlayer)) {
+        UI.unbindClick("tr td");
+        UI.hideHumanMessage();
+        if (Game.checkGameOver(UI.currentPlayerUI) === false) {
+          callback(Game.playGame);
+        }
+      }
+      else {
+        return;
+      }
+    },
+
+    computerChoice: function(callback) {
+      Computer.chooseTheBestSpot(UI.currentPlayer);
+      if (Game.checkGameOver(UI.currentPlayer) === false) {
+        callback(Game.playGame);
+      }
+    },
+
+    playGame: function() {
+      if(UI.gameType === ".player") {
+        if (Game.goFirst === "y") {
+          UI.humanPlay();
+        }
+        else if (Game.goFirst === "n") {
+          Game.changeCurrentPlayer(UI.currentPlayer);
+          UI.computerPlay();
+        }
+      }
+      else { 
+        if (Game.goFirst !== "y") {
+          Game.changeCurrentPlayer(UI.currentPlayer);
+        }
+        UI.humanPlay();
+      }
+    }
+  };
+  window.Game = Game;
+})();
---
+++ public/javascripts/UI.js
@@ -0,0 +1,174 @@
+(function() {
+  var UI = {
+    gameType: null,
+    currentPlayer: null,
+
+    unbindClick: function(element) {
+      $(element).unbind("click");
+    },
+
+    visualAfterGameOver: function() {
+      this.unbindClick("tr td");
+      this.toggleDisplayedButton(".btn-new", ".btn-restart");
+      this.hideComputerMessage();
+      this.hideHumanMessage();
+      this.restartGame();
+    },
+
+    visualWhenGameOver: function(currentPlayer) {
+      if(GameRules.gameWin(GameBoard)) {
+        this.winMessage(Game.winner(currentPlayer));
+        this.visualAfterGameOver();
+      }
+      else if(GameRules.gameTie(GameBoard)) {
+        this.tieMessage();
+        this.visualAfterGameOver();
+      }
+    },
+
+    showComputerMessage: function(callback) {
+      $("#Computer").show("fast", function() {
+        callback(UI.humanPlay);
+      });
+    },
+
+    hideComputerMessage: function() {
+      $("#Computer").hide();
+    },
+
+    showHumanMessage: function() {
+      $("#Human").show();
+    },
+
+    hideHumanMessage: function() {
+      $("#Human").hide();
+    },
+
+    changeHumanMessage: function() {
+      if (UI.gameType === ".player") {
+        UI.setTextContents("Human", "Click a spot you want.");
+      }
+      else {
+        UI.setTextContents("Human", "Player '" + Game.currentPlayer + "' click a spot you want.");
+      }
+    },
+
+    winMessage: function(winner) {
+      alert("Congratulations.\n"+ winner +" win!!");
+    },
+
+    tieMessage: function() {
+      alert("Game is tied.\nGame Over.");
+    },
+
+    spotErrorMessage: function() {
+      alert("That is not an available spot.\nPlease choose a different spot.");
+    },
+
+    inputErrorMessage: function() {
+      alert("You have to choose 'y' or 'n'.");
+    },
+
+    askFirstMove: function() {
+      return prompt("Do you require the first move? (y/n)");
+    },
+
+    removeText: function(element) {
+      $(element).empty();
+    },
+
+    setTextContents: function(elementID, TextContents) {
+      $("#" + elementID).text(TextContents);
+      $.post("/game/chosen_spot/", elementID);
+    },
+
+    getTextContents: function(elementID) {
+      return $("#" + elementID).text()
+    },
+
+    toggleDisplayedButtons: function(hidebutton, showbutton) {
+      $(hidebutton).hide();
+      $(hidebutton).unbind();
+      $(showbutton).show()
+    },
+
+    hideButtons: function(button1, button2, button3) {
+      $(button1).hide();
+      $(button2).hide();
+      $(button3).hide();
+    },
+
+    clickSpot: function(callback) {
+      $("tr td").click(function(e) {
+        if (UI.gameType === ".player") {
+          callback(e.target.id, UI.computerPlay);
+        }
+        else {
+          callback(e.target.id, UI.humanPlay);
+        }
+      });
+    },
+
+    clickButton: function(button, callback) {
+      $(button).click(function(e) {
+        if (button == ".player" || button == ".players") {
+          UI.gameType = button;
+          return UI.choiceMark();
+        }
+        else if (button == "#Xmark" || button == "#Omark") {
+          UI.currentPlayer = UI.getTextContents(e.target.id);
+          $.post("/game/choice_mark/", UI.getTextContents(e.target.id));
+          UI.toggleDisplayedButton(".playerMark", ".game");
+        }
+        else if (button == ".btn-new") {
+          UI.hideButton(".game")
+          UI.unbindClick("button");
+          UI.unbindClick("tr td");
+          UI.resetGame();
+        }
+        else if (button == ".btn-restart") {
+          UI.hideButton(".game")
+          UI.unbindClick("button");
+          UI.resetGame();
+        }
+        e.stopPropagation();
+        callback();
+      });
+    },
+
+    gameOver: function() {
+      console.log( $.get("/game/:gameOver"))
+    },
+
+    humanPlay: function() {
+      UI.showHumanMessage();
+      UI.clickSpot(Game.humanChoice);
+    },
+
+    computerPlay: function() {
+      UI.showComputerMessage(Game.computerChoice);
+    },
+
+    gameStart: function() {
+      Game.firstMove();
+      Game.playGame();
+    },
+
+    choiceMark: function() {
+      UI.toggleDisplayedButtons(".menu", ".playerMark");
+      UI.clickButton("#Xmark", UI.gameStart);
+      UI.clickButton("#Omark", UI.gameStart);
+    },
+
+    gameMenu: function() {
+      UI.toggleDisplayedButtons(".btn-start", ".menu");
+      UI.clickButton(".player");
+    },
+
+    gameMain: function() {
+      this.hideButtons(".menu", ".playerMark", ".game");
+      UI.clickButton(".btn-start", UI.gameMenu);
+    }
+  }
+  window.UI = UI;
+})();
