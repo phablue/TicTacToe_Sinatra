a4acc243ba0718c0589d9c40add531e9dee7ea1c
hyunahpark <phablue@gmail.com>
2014-03-27 16:02:09 -0500

    delete needless function

--- lib/Computer.rb
+++ lib/Computer.rb
@@ -1,15 +1,13 @@
 require_relative "game_rules"
 
 class Computer
-	def initialize(output)
+	def initialize
 		@rules = GameRules.new
-		@output = output
 	end
 
 	def choose_the_best_spot(board, current_player)
-		show_computer_msg
 		choice = minimax(board, current_player)[1]
-		board.mark_choice_spot(choice, current_player)
+		board.mark_choice_spot(choice-1, current_player)
 	end
 
 	def minimax(board, current_player, level = 0)
@@ -28,14 +26,6 @@ class Computer
 		 	end
 		 return [best_point, best_spot]
 	 end
-
-	def computer_msg
-		"\nPlease wait to computer play..\n\n"
-	end
-
-	def show_computer_msg
-		@output.print_out(computer_msg)
-	end
 
 	def get_point(board, current_player, level)
 		return (1.0 / -level) if @rules.game_win(board)
