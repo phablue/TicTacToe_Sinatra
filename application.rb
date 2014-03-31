18878da300173ebaf26ed0d85ee43231a3a5c5d5
hyunahpark <phablue@gmail.com>
2014-03-31 14:15:49 -0500

    add session

--- application.rb
+++ application.rb
@@ -2,13 +2,14 @@ require "json"
 require "sinatra"
 require "./lib/game"
 
+use Rack::Session::Pool
+
 configure do
   set :game => Game.new
 end
 
 before do
   @game = settings.game
-  @current_player
 end
 
 get "/" do
@@ -16,7 +17,7 @@ get "/" do
 end
 
 get "/game/" do
-  {:game_over => @game.rules.game_over()}.to_json
+  {:game_over => @game.rules.game_over(@game.board)}.to_json
 end
 
 post "/game/human/" do
@@ -26,9 +27,10 @@ post "/game/human/" do
 end
 
 post "/game/computer/" do
-  @current_player = params[:current_player]
+  session[:current_player] = params[:current_player]
 end
 
 get "/game/computer/" do
-  {:computer_choice => @game.computer.choose_the_best_spot(@game.board, @current_player)}.to_json
+  current_player = session[:current_player]
+  {:computer_choice => @game.computer.choose_the_best_spot(@game.board, current_player)}.to_json
 end
