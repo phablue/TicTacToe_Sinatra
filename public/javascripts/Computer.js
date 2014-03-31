496f9b14e78532909cea51954355baa633abecfc
hyunahpark <phablue@gmail.com>
2014-03-31 09:59:36 -0500

    fixed syntax in Computer

--- public/javascripts/Computer.js
+++ public/javascripts/Computer.js
@@ -2,7 +2,7 @@
   var Computer = {
     chooseTheBestSpot: function(currentPlayer) {
       $.getJSON("/game/").done(function(data) {
-        var chosenSpot $.parseJSON(data.computer_choice);
+        var chosenSpo = $.parseJSON(data.computer_choice);
       });
       console.log(chosenSpot)
       Human.markChosenSpot(GameBoard, chosenSpot-1, currentPlayer);
